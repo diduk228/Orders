ea04150a8bbc44b45d88713a9fc104ecc46754e0
duduk228 <danil.glebov.1999@mail.ru>
2021-12-01 23:24:50 +0700

    Добавил иницилизацию таблицы

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
+    init_table();
 }
 
 MainWindow::~MainWindow()
@@ -26,4 +26,51 @@ void MainWindow::on_delete_products_triggered()
     DeleteProducts *del = new DeleteProducts(this);
     this->close();
     del->show();
+}
+
+void MainWindow::on_add_goods_triggered()
+{
+    AddGoods *add = new AddGoods(this);
+    this->close();
+    add->show();
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    MenuBasket *add = new MenuBasket(this);
+    this->close();
+    add->show();
+}
+
+void MainWindow::init_table()
+{
+    int count =  get_count_products();
+    ui->tableWidget->setColumnCount(2);
+    ui->tableWidget->setRowCount(count);
+    QVector<QVector<QString>> vec =  get_all();
+    for(int i = 0; i < count; i++)
+    {
+            QTableWidgetItem *itm = new QTableWidgetItem(tr("%1").arg(vec[i][1]));
+            ui->tableWidget->setItem(i,0, itm);
+            addBtnAt(i, 1);
+
+    }
+}
+
+void MainWindow::addBtnAt(int row_number, int column_number)
+{
+    // Create a widget that will contain a checkbox
+     QWidget *checkBoxWidget = new QWidget();
+     QPushButton *quitBtn = new QPushButton();      // We declare and initialize the checkbox
+     quitBtn->setText("Выбрать");
+     QHBoxLayout *layoutCheckBox = new QHBoxLayout(checkBoxWidget); // create a layer with reference to the widget
+     layoutCheckBox->addWidget(quitBtn);            // Set the checkbox in the layer
+     layoutCheckBox->setAlignment(Qt::AlignCenter);  // Center the checkbox
+     layoutCheckBox->setContentsMargins(0,0,0,0);    // Set the zero padding
+     /* Check on the status of odd if an odd device,
+       * exhibiting state of the checkbox in the Checked, Unchecked otherwise
+       * */
+
+
+      ui->tableWidget->setCellWidget(row_number,column_number, checkBoxWidget);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,8 @@
 #include "basedata.h"
 #include "processorderswindow.h"
 #include "deleteproducts.h"
+#include "addgoods.h"
+#include "menubasket.h"
 namespace Ui {
 class MainWindow;
 }
@@ -21,8 +23,14 @@ private:
     Ui::MainWindow *ui;
 
 private slots:
-    void on_process_products_2_triggered(); // открывает меню обработки заказов
-    void on_delete_products_triggered();
+    void on_process_products_2_triggered(); // Открывает меню обработки заказов
+    void on_delete_products_triggered();    // Открывает меню удаления товаров
+    void on_add_goods_triggered();          // Открывает меню добавления товаров
+    void on_pushButton_3_clicked();         // Открывает Корзину
+
+private:
+    void init_table();  // Иницилизирует таблицу в главном меню
+    void addBtnAt(int row_number, int column_number);  //добавляет в таблицу кнопки
 };
 
 #endif // MAINWINDOW_H
