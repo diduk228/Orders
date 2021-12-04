55552ea7d1d19231e062ed2b0cc10715986b2d06
duduk228 <danil.glebov.1999@mail.ru>
2021-12-04 15:18:31 +0700

    Добавил тест проверки покупки товара

--- Test/tst_mytest.cpp
+++ Test/tst_mytest.cpp
@@ -29,6 +29,7 @@ private slots:
 
 
     void test_add_to_basket();
+    void test_buy_poduct();
 
 };
 
@@ -101,7 +102,19 @@ void MyTest::test_add_to_basket()
     quitBtn->click();
     QVector<QString> data = bs->get_ids();
     QCOMPARE(data[data.length()-1], bs->get_m_current_product_id());
+}
 
+void MyTest::test_buy_poduct()
+{
+    //В файле текущего товара должен быть 19!!!
+    QWidget *w = new QWidget();
+    MainWindow *bs = new MainWindow(w);
+    int count = bs->get_count_orders();
+    bs->set_count_buy("1");
+    QPushButton *quitBtn = bs->get_ui_btn_buy_product();
+    quitBtn->click();
+    int count2 = bs->get_count_orders() - count;
+    QCOMPARE(count2, 1);
 }
 
 
